6c0d07a6918af8440e30c957f71eda6bda22978c
Usama <uusman004@gmail.com>
2024-06-24 17:03:12 +0500

    Setup: IEEE Paper

---
+++ Templates/APA7/app.py
@@ -0,0 +1,2 @@
+def APA7():
+    retrun "Hello this is APA Format"
--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import importlib
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/template", methods=["POST"])
+def get_template():
+    data = request.get_json()
+    template_name = data.get('templateName')
+    title = data.get('title')
+    
+
+    if template_name:
+        try:
+            module = importlib.import_module(f"Templates.{template_name}.app")
+            template_func = getattr(module, template_name)
+            response = template_func(title)
+            return jsonify({"message": response}), 200
+        
+        except (ModuleNotFoundError, AttributeError) as e:
+            return jsonify({"error": str(e)}), 400
+    else:
+        return jsonify({"error": "templateName not provided"}), 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
